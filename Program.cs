4acc6e2735e2abe4fb3df543c37dc7929f07ac96
jotaro4u <kothaker135@gmail.com>
2025-02-03 09:25:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void AP(int a1, int d)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(a1 + i * d + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            AP(1, 4);
+
+            Console.ReadLine();
+        }
+    }
+}
